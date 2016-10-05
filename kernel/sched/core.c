b3a3dfd6333d76cbc6259e10875b63c4a8ec301d
Peter Zijlstra <peterz@infradead.org>
2016-10-05 17:59:32 +0200

    sched/core: Avoid double update_rq_clock() in move_queued_task()
    Address this case:
    
      WARNING: CPU: 0 PID: 2070 at ../kernel/sched/core.c:109 update_rq_clock+0x74/0x80
      rq->clock_update_flags & RQCF_UPDATED
    
      Call Trace:
      update_rq_clock()
      move_queued_task()
      __set_cpus_allowed_ptr()
      ...
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    kernel/sched/core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -993,7 +993,7 @@ static struct rq *move_queued_task(struct rq *rq, struct rq_flags *rf,
 	lockdep_assert_held(&rq->lock);
 
 	p->on_rq = TASK_ON_RQ_MIGRATING;
-	dequeue_task(rq, p, 0);
+	dequeue_task(rq, p, DEQUEUE_NOCLOCK);
 #ifdef CONFIG_SCHED_WALT
 	double_lock_balance(rq, cpu_rq(new_cpu));
 	set_task_cpu(p, new_cpu);
@@ -1038,6 +1038,7 @@ static struct rq *__migrate_task(struct rq *rq, struct rq_flags *rf,
 	if (!cpumask_test_cpu(dest_cpu, &p->cpus_allowed))
 		return rq;
 
+	update_rq_clock(rq);
 	rq = move_queued_task(rq, rf, p, dest_cpu);
 
 	return rq;
