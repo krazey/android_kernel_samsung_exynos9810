4bccca0d5d07de60805c10027c08fbb97b40a41b
Josh Poimboeuf <jpoimboe@redhat.com>
2016-10-20 11:34:40 -0500

    x86/entry/unwind: Create stack frames for saved interrupt registers
    With frame pointers, when a task is interrupted, its stack is no longer
    completely reliable because the function could have been interrupted
    before it had a chance to save the previous frame pointer on the stack.
    So the caller of the interrupted function could get skipped by a stack
    trace.
    
    This is problematic for live patching, which needs to know whether a
    stack trace of a sleeping task can be relied upon.  There's currently no
    way to detect if a sleeping task was interrupted by a page fault
    exception or preemption before it went to sleep.
    
    Another issue is that when dumping the stack of an interrupted task, the
    unwinder has no way of knowing where the saved pt_regs registers are, so
    it can't print them.
    
    This solves those issues by encoding the pt_regs pointer in the frame
    pointer on entry from an interrupt or an exception.
    
    This patch also updates the unwinder to be able to decode it, because
    otherwise the unwinder would be broken by this change.
    
    Note that this causes a change in the behavior of the unwinder: each
    instance of a pt_regs on the stack is now considered a "frame".  So
    callers of unwind_get_return_address() will now get an occasional
    'regs->ip' address that would have previously been skipped over.
    
    Suggested-by: Andy Lutomirski <luto@amacapital.net>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/8b9f84a21e39d249049e0547b559ff8da0df0988.1476973742.git.jpoimboe@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Conflicts:
    arch/x86/entry/calling.h
    arch/x86/entry/entry_64.S
    arch/x86/kernel/unwind_frame.c

--- arch/x86/entry/calling.h
+++ arch/x86/entry/calling.h
@@ -194,20 +194,23 @@ For 32-bit we have the following conventions - kernel is built with
 	.endm
 
 /*
- * Mitigate Spectre v1 for conditional swapgs code paths.
+ * This is a sneaky trick to help the unwinder find pt_regs on the stack.  The
+ * frame pointer is replaced with an encoded pointer to pt_regs.  The encoding
+ * is just setting the LSB, which makes it an invalid stack address and is also
+ * a signal to the unwinder that it's a pt_regs pointer in disguise.
  *
- * FENCE_SWAPGS_USER_ENTRY is used in the user entry swapgs code path, to
- * prevent a speculative swapgs when coming from kernel space.
- *
- * FENCE_SWAPGS_KERNEL_ENTRY is used in the kernel entry non-swapgs code path,
- * to prevent the swapgs from getting speculatively skipped when coming from
- * user space.
+ * NOTE: This macro must be used *after* SAVE_EXTRA_REGS because it corrupts
+ * the original rbp.
  */
-.macro FENCE_SWAPGS_USER_ENTRY
-	ALTERNATIVE "", "lfence", X86_FEATURE_FENCE_SWAPGS_USER
-.endm
-.macro FENCE_SWAPGS_KERNEL_ENTRY
-	ALTERNATIVE "", "lfence", X86_FEATURE_FENCE_SWAPGS_KERNEL
+.macro ENCODE_FRAME_POINTER ptregs_offset=0
+#ifdef CONFIG_FRAME_POINTER
+	.if \ptregs_offset
+		leaq \ptregs_offset(%rsp), %rbp
+	.else
+		mov %rsp, %rbp
+	.endif
+	orq	$0x1, %rbp
+#endif
 .endm
 
 #endif /* CONFIG_X86_64 */
