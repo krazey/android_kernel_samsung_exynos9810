0fb9340368e261eaae2d7d8234423fcd35458e3f
Christoph Hellwig <hch@lst.de>
2016-10-28 08:48:16 -0600

    block: better op and flags encoding
    Now that we don't need the common flags to overflow outside the range
    of a 32-bit type we can encode them the same way for both the bio and
    request fields.  This in addition allows us to place the operation
    first (and make some room for more ops while we're at it) and to
    stop having to shift around the operation values.
    
    In addition this allows passing around only one value in the block layer
    instead of two (and eventuall also in the file systems, but we can do
    that later) and thus clean up a lot of code.
    
    Last but not least this allows decreasing the size of the cmd_flags
    field in struct request to 32-bits.  Various functions passing this
    value could also be updated, but I'd like to avoid the churn for now.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Conflicts:
    	block/blk-core.c
    	block/blk-exynos-mq.c
    	include/linux/blkdev.h
    	kernel/trace/blktrace.c
    
    Signed-off-by: krazey <admin@krazey.de>

--- kernel/trace/blktrace.c
+++ kernel/trace/blktrace.c
@@ -1913,14 +1913,14 @@ void blk_dump_cmd(char *buf, struct request *rq)
 
 SIO_PATCH_VERSION(ftrace_discard_bugfix, 1, 0, "");
 
-void blk_fill_rwbs(char *rwbs, int op, u32 rw, int bytes)
+void blk_fill_rwbs(char *rwbs, unsigned int op, int bytes)
 {
 	int i = 0;
 
-	if (rw & REQ_PREFLUSH)
+	if (op & REQ_PREFLUSH)
 		rwbs[i++] = 'F';
 
-	switch (op) {
+	switch (op & REQ_OP_MASK) {
 	case REQ_OP_DISCARD:
 		rwbs[i++] = 'D';
 		break;
@@ -1942,13 +1942,13 @@ void blk_fill_rwbs(char *rwbs, int op, u32 rw, int bytes)
 		rwbs[i++] = 'N';
 	}
 
-	if (rw & REQ_FUA)
+	if (op & REQ_FUA)
 		rwbs[i++] = 'F';
-	if (rw & REQ_RAHEAD)
+	if (op & REQ_RAHEAD)
 		rwbs[i++] = 'A';
-	if (rw & REQ_SYNC)
+	if (op & REQ_SYNC)
 		rwbs[i++] = 'S';
-	if (rw & REQ_META)
+	if (op & REQ_META)
 		rwbs[i++] = 'M';
 
 	rwbs[i] = '\0';
