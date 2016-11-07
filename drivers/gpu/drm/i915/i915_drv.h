3df4001ed14aee627f9bc8d556ef281d0c9a896f
Robert Bragg <robert@sixbynine.org>
2016-11-07 19:49:54 +0000

    drm/i915: Add dev.i915.perf_stream_paranoid sysctl option
    Consistent with the kernel.perf_event_paranoid sysctl option that can
    allow non-root users to access system wide cpu metrics, this can
    optionally allow non-root users to access system wide OA counter metrics
    from Gen graphics hardware.
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Reviewed-by: Sourab Gupta <sourab.gupta@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161107194957.3385-9-robert@sixbynine.org

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2198,6 +2198,7 @@ struct drm_i915_private {
 		bool initialized;
 
 		struct kobject *metrics_kobj;
+		struct ctl_table_header *sysctl_header;
 
 		struct mutex lock;
 		struct list_head streams;
