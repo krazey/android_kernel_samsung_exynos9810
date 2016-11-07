14838e18c29f4c98d239071e8ef280e7be6c8bad
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2016-11-07 09:29:20 +0000

    drm/i915: More assorted dev_priv cleanups
    A small selection of macros which can only accept dev_priv from
    now on and a resulting trickle of fixups.
    
    v2: Keep original order. (Ville Syrjala)
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: David Weinehall <david.weinehall@linux.intel.com>

--- drivers/gpu/drm/i915/intel_psr.c
+++ drivers/gpu/drm/i915/intel_psr.c
@@ -433,7 +433,7 @@ void intel_psr_enable(struct intel_dp *intel_dp)
 	struct drm_device *dev = intel_dig_port->base.base.dev;
 	struct drm_i915_private *dev_priv = to_i915(dev);
 
-	if (!HAS_PSR(dev)) {
+	if (!HAS_PSR(dev_priv)) {
 		DRM_DEBUG_KMS("PSR not supported on this platform\n");
 		return;
 	}
