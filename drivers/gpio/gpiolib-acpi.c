a5b9c553b3ae2ddb2d2bfe60be42d017a46b2aaa
Arnd Bergmann <arnd@arndb.de>
2016-11-08 14:40:06 +0100

    ACPI / gpio: avoid warning for gpio hogging code
    The newly added acpi_gpiochip_scan_gpios function produces a few harmless
    warnings:
    
    drivers/gpio/gpiolib-acpi.c: In function ‘acpi_gpiochip_add’:
    drivers/gpio/gpiolib-acpi.c:925:7: error: ‘dflags’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
    drivers/gpio/gpiolib-acpi.c:925:9: error: ‘lflags’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
    
    The problem is that he compiler cannot know that a negative return value
    from fwnode_property_read_u32_array() or acpi_gpiochip_pin_to_gpio_offset()
    implies that the IS_ERR(gpio_desc) is true, as the value could in theory
    be below -MAX_ERRNO.
    
    The function already initializes its output values to zero, and moving
    that intialization a little higher up ensures that we can never have
    uninitialized data in the caller.
    
    Fixes: c80f1ba75df2 ("ACPI / gpio: Add hogging support")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-acpi.c
+++ drivers/gpio/gpiolib-acpi.c
@@ -871,6 +871,10 @@ static struct gpio_desc *acpi_gpiochip_parse_own_gpio(
 	u32 gpios[2];
 	int ret;
 
+	*lflags = 0;
+	*dflags = 0;
+	*name = NULL;
+
 	ret = fwnode_property_read_u32_array(fwnode, "gpios", gpios,
 					     ARRAY_SIZE(gpios));
 	if (ret < 0)
@@ -883,10 +887,6 @@ static struct gpio_desc *acpi_gpiochip_parse_own_gpio(
 	desc = gpiochip_get_desc(chip, ret);
 	if (IS_ERR(desc))
 		return desc;
-
-	*lflags = 0;
-	*dflags = 0;
-	*name = NULL;
 
 	if (gpios[1])
 		*lflags |= GPIO_ACTIVE_LOW;
