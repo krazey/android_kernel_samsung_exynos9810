0a2575ed1ed10172ca816bfe46f8b922621bb171
Marek Szyprowski <m.szyprowski@samsung.com>
2016-11-09 12:24:00 -0200

    [media] exynos-gsc: Add missing newline char in debug messages
    Fix missing newline char in debug messages.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/exynos-gsc/gsc-core.c
+++ drivers/media/platform/exynos-gsc/gsc-core.c
@@ -1149,7 +1149,7 @@ static int gsc_runtime_resume(struct device *dev)
 	struct gsc_dev *gsc = dev_get_drvdata(dev);
 	int ret = 0;
 
-	pr_debug("gsc%d: state: 0x%lx", gsc->id, gsc->state);
+	pr_debug("gsc%d: state: 0x%lx\n", gsc->id, gsc->state);
 
 	ret = clk_prepare_enable(gsc->clock);
 	if (ret)
@@ -1171,7 +1171,7 @@ static int gsc_runtime_suspend(struct device *dev)
 	if (!ret)
 		clk_disable_unprepare(gsc->clock);
 
-	pr_debug("gsc%d: state: 0x%lx", gsc->id, gsc->state);
+	pr_debug("gsc%d: state: 0x%lx\n", gsc->id, gsc->state);
 	return ret;
 }
 #endif
