195948c0fddf1d9199512cc456d562338d99b3fe
David Sterba <dsterba@suse.com>
2016-11-09 17:43:38 +0100

    btrfs: introduce helpers for updating eb uuids
    The fsid and chunk tree uuid are always located in the first page,
    we don't need the to use write_extent_buffer.
    
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    fs/btrfs/extent_io.h

--- fs/btrfs/extent_io.h
+++ fs/btrfs/extent_io.h
@@ -404,6 +404,9 @@ void read_extent_buffer(const struct extent_buffer *eb, void *dst,
 int read_extent_buffer_to_user_nofault(const struct extent_buffer *eb,
 				       void __user *dst, unsigned long start,
 				       unsigned long len);
+void write_extent_buffer_fsid(struct extent_buffer *eb, const void *src);
+void write_extent_buffer_chunk_tree_uuid(struct extent_buffer *eb,
+		const void *src);
 void write_extent_buffer(struct extent_buffer *eb, const void *src,
 			 unsigned long start, unsigned long len);
 void copy_extent_buffer(struct extent_buffer *dst, struct extent_buffer *src,
