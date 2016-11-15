7a7788fccff6fb2ff85640c166a4eb90a660adca
John Youn <johnyoun@synopsys.com>
2016-11-15 13:08:59 +0200

    usb: dwc3: gadget: clear events in top-half handler
    Now that all the infrastructure is in place, we can
    clear events in the top-half handler in order to
    bring IRQ line low ASAP.
    
    This is also a necessary step in order to implement
    workaround for known erratum in follow-up patches.
    
    Signed-off-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Signed-off-by: krazey <admin@krazey.de>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -3462,8 +3462,6 @@ static irqreturn_t dwc3_process_event_buf(struct dwc3_event_buffer *evt)
 		 */
 		evt->lpos = (evt->lpos + 4) % evt->length;
 		left -= 4;
-
-		dwc3_writel(dwc->regs, DWC3_GEVNTCOUNT(0), 4);
 	}
 
 	evt->count = 0;
@@ -3538,6 +3536,8 @@ static irqreturn_t dwc3_check_event_buf(struct dwc3_event_buffer *evt)
 
 	if (amount < count)
 		memcpy(evt->cache, evt->buf, count - amount);
+
+	dwc3_writel(dwc->regs, DWC3_GEVNTCOUNT(0), count);
 
 	return IRQ_WAKE_THREAD;
 }
