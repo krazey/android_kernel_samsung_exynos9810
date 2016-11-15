d834a926a8d198b794919b818feb8bc82868b513
Eric Whitney <enwlinux@gmail.com>
2016-11-14 21:56:48 -0500

    ext4: remove parameter from ext4_xattr_ibody_set()
    The parameter "handle" isn't used.
    
    Signed-off-by: Eric Whitney <enwlinux@gmail.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    fs/ext4/xattr.c

--- fs/ext4/xattr.c
+++ fs/ext4/xattr.c
@@ -1122,7 +1122,7 @@ int ext4_xattr_ibody_inline_set(handle_t *handle, struct inode *inode,
 	return 0;
 }
 
-static int ext4_xattr_ibody_set(handle_t *handle, struct inode *inode,
+static int ext4_xattr_ibody_set(struct inode *inode,
 				struct ext4_xattr_info *i,
 				struct ext4_xattr_ibody_find *is)
 {
@@ -1237,7 +1237,7 @@ ext4_xattr_set_handle(handle_t *handle, struct inode *inode, int name_index,
 	}
 	if (!value) {
 		if (!is.s.not_found)
-			error = ext4_xattr_ibody_set(handle, inode, &i, &is);
+			error = ext4_xattr_ibody_set(inode, &i, &is);
 		else if (!bs.s.not_found)
 			error = ext4_xattr_block_set(handle, inode, &i, &bs);
 	} else {
@@ -1248,7 +1248,7 @@ ext4_xattr_set_handle(handle_t *handle, struct inode *inode, int name_index,
 		if (!bs.s.not_found && ext4_xattr_value_same(&bs.s, &i))
 			goto cleanup;
 
-		error = ext4_xattr_ibody_set(handle, inode, &i, &is);
+		error = ext4_xattr_ibody_set(inode, &i, &is);
 		if (!error && !bs.s.not_found) {
 			i.value = NULL;
 			error = ext4_xattr_block_set(handle, inode, &i, &bs);
@@ -1267,7 +1267,7 @@ ext4_xattr_set_handle(handle_t *handle, struct inode *inode, int name_index,
 				i.value = NULL;
 				SE_LOG(">>> Don't trust next log. %s : ino(%lu)",
 						__func__, inode->i_ino);
-				error = ext4_xattr_ibody_set(handle, inode, &i,
+				error = ext4_xattr_ibody_set(inode, &i,
 							     &is);
 			}
 		}
@@ -1407,7 +1407,7 @@ static int ext4_xattr_move_to_block(handle_t *handle, struct inode *inode,
 		goto out;
 
 	/* Remove the chosen entry from the inode */
-	error = ext4_xattr_ibody_set(handle, inode, &i, is);
+	error = ext4_xattr_ibody_set(inode, &i, is);
 	if (error)
 		goto out;
 
