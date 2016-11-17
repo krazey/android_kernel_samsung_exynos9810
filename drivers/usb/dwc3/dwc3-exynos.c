cfb38e460896d8f0fdb4ea7e85b8fd5d6cf9df55
Arnd Bergmann <arnd@arndb.de>
2016-11-17 17:13:48 +0530

    usb: dwc3: Do not set dma coherent mask
    The dma mask is correctly set up by the DT probe function, no
    need to override it any more.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Sriram Dash <sriram.dash@nxp.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    drivers/usb/dwc3/dwc3-exynos.c

--- drivers/usb/dwc3/dwc3-exynos.c
+++ drivers/usb/dwc3/dwc3-exynos.c
@@ -21,7 +21,6 @@
 #include <linux/slab.h>
 #include <linux/platform_device.h>
 #include <linux/mutex.h>
-#include <linux/dma-mapping.h>
 #include <linux/clk.h>
 #include <linux/usb/otg.h>
 #include <linux/usb/usb_phy_generic.h>
@@ -492,19 +491,6 @@ static int dwc3_exynos_probe(struct platform_device *pdev)
 	exynos = devm_kzalloc(dev, sizeof(*exynos), GFP_KERNEL);
 	if (!exynos)
 		return -ENOMEM;
-
-	/*
-	 * Right now device-tree probed devices don't get dma_mask set.
-	 * Since shared usb code relies on it, set it here for now.
-	 * Once we move to full device tree support this will vanish off.
-	 */
-#if IS_ENABLED(CONFIG_ZONE_DMA)
-	ret = dma_coerce_mask_and_coherent(dev, DMA_BIT_MASK(32));
-#else
-	ret = dma_coerce_mask_and_coherent(dev, DMA_BIT_MASK(36));
-#endif
-	if (ret)
-		return ret;
 
 	platform_set_drvdata(pdev, exynos);
 
