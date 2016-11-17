f99bc29f85aa2762b4a048e5f4f4d6a624e95728
Johannes Thumshirn <jthumshirn@suse.de>
2016-11-17 10:31:16 +0100

    scsi: libfc: don't set FC_RQST_STATE_DONE before calling fc_bsg_jobdone()
    Don't set FC_RQST_STATE_DONE before calling fc_bsg_jobdone() as
    fc_bsg_jobdone() calls blk_complete_requeust() which raises a soft-IRQ
    that ends up in fc_bsg_sofirq_done() and fc_bsg_softirq_done() sets the
    FC_RQST_STATE_DONE flag.
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/libfc/fc_lport.c
+++ drivers/scsi/libfc/fc_lport.c
@@ -1911,7 +1911,6 @@ static void fc_lport_bsg_resp(struct fc_seq *sp, struct fc_frame *fp,
 		bsg_reply->result = (PTR_ERR(fp) == -FC_EX_CLOSED) ?
 			-ECONNABORTED : -ETIMEDOUT;
 		job->reply_len = sizeof(uint32_t);
-		job->state_flags |= FC_RQST_STATE_DONE;
 		fc_bsg_jobdone(job, bsg_reply->result,
 			       bsg_reply->reply_payload_rcv_len);
 		kfree(info);
@@ -1947,7 +1946,6 @@ static void fc_lport_bsg_resp(struct fc_seq *sp, struct fc_frame *fp,
 			bsg_reply->reply_payload_rcv_len =
 				job->reply_payload.payload_len;
 		bsg_reply->result = 0;
-		job->state_flags |= FC_RQST_STATE_DONE;
 		fc_bsg_jobdone(job, bsg_reply->result,
 			       bsg_reply->reply_payload_rcv_len);
 		kfree(info);
