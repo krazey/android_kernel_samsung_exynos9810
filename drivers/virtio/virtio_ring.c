3312957f8b83476c3c6eff74f54f9b943b261b2e
Gonglei <arei.gonglei@huawei.com>
2016-11-22 13:51:50 +0800

    virtio_ring: fix complaint by sparse
     # make C=2 CF="-D__CHECK_ENDIAN__" ./drivers/virtio/
    
    drivers/virtio/virtio_ring.c:423:19: warning: incorrect type in assignment (different base types)
    drivers/virtio/virtio_ring.c:423:19:    expected unsigned int [unsigned] [assigned] i
    drivers/virtio/virtio_ring.c:423:19:    got restricted __virtio16 [usertype] next
    drivers/virtio/virtio_ring.c:423:19: warning: incorrect type in assignment (different base types)
    drivers/virtio/virtio_ring.c:423:19:    expected unsigned int [unsigned] [assigned] i
    drivers/virtio/virtio_ring.c:423:19:    got restricted __virtio16 [usertype] next
    drivers/virtio/virtio_ring.c:423:19: warning: incorrect type in assignment (different base types)
    drivers/virtio/virtio_ring.c:423:19:    expected unsigned int [unsigned] [assigned] i
    drivers/virtio/virtio_ring.c:423:19:    got restricted __virtio16 [usertype] next
    drivers/virtio/virtio_ring.c:604:39: warning: incorrect type in initializer (different base types)
    drivers/virtio/virtio_ring.c:604:39:    expected unsigned short [unsigned] [usertype] nextflag
    drivers/virtio/virtio_ring.c:604:39:    got restricted __virtio16
    drivers/virtio/virtio_ring.c:612:33: warning: restricted __virtio16 degrades to integer
    
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -420,7 +420,7 @@ unmap_release:
 		if (i == err_idx)
 			break;
 		vring_unmap_one(vq, &desc[i]);
-		i = vq->vring.desc[i].next;
+		i = virtio16_to_cpu(_vq->vdev, vq->vring.desc[i].next);
 	}
 
 	if (indirect)
@@ -599,7 +599,7 @@ EXPORT_SYMBOL_GPL(virtqueue_kick);
 static void detach_buf(struct vring_virtqueue *vq, unsigned int head)
 {
 	unsigned int i, j;
-	u16 nextflag = cpu_to_virtio16(vq->vq.vdev, VRING_DESC_F_NEXT);
+	__virtio16 nextflag = cpu_to_virtio16(vq->vq.vdev, VRING_DESC_F_NEXT);
 
 	/* Clear data ptr. */
 	vq->desc_state[head].data = NULL;
