562c24a35f8a2a801da3ffa16913c104843900c5
Florian Fainelli <f.fainelli@gmail.com>
2016-11-22 13:55:31 -0800

    ethtool: Protect {get, set}_phy_tunable with PHY device mutex
    PHY drivers should be able to rely on the caller of {get,set}_tunable to
    have acquired the PHY device mutex, in order to both serialize against
    concurrent calls of these functions, but also against PHY state machine
    changes. All ethtool PHY-level functions do this, except
    {get,set}_tunable, so we make them consistent here as well.
    
    We need to update the Microsemi PHY driver in the same commit to avoid
    introducing either deadlocks, or lack of proper locking.
    
    Fixes: 968ad9da7e0e ("ethtool: Implements ETHTOOL_PHY_GTUNABLE/ETHTOOL_PHY_STUNABLE")
    Fixes: 310d9ad57ae0 ("net: phy: Add downshift get/set support in Microsemi PHYs driver")
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Allan W. Nielsen <allan.nielsen@microsemi.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/ethtool.c
+++ net/core/ethtool.c
@@ -2492,7 +2492,9 @@ static int get_phy_tunable(struct net_device *dev, void __user *useraddr)
 	data = kmalloc(tuna.len, GFP_USER);
 	if (!data)
 		return -ENOMEM;
+	mutex_lock(&phydev->lock);
 	ret = phydev->drv->get_tunable(phydev, &tuna, data);
+	mutex_unlock(&phydev->lock);
 	if (ret)
 		goto out;
 	useraddr += sizeof(tuna);
@@ -2527,7 +2529,9 @@ static int set_phy_tunable(struct net_device *dev, void __user *useraddr)
 	ret = -EFAULT;
 	if (copy_from_user(data, useraddr, tuna.len))
 		goto out;
+	mutex_lock(&phydev->lock);
 	ret = phydev->drv->set_tunable(phydev, &tuna, data);
+	mutex_unlock(&phydev->lock);
 
 out:
 	kfree(data);
