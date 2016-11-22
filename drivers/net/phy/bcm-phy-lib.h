dfd569574b8269695d1fa05634bb2a556c7eb553
Florian Fainelli <f.fainelli@gmail.com>
2016-11-22 11:40:54 -0800

    net: phy: broadcom: Move bcm54xx_auxctl_{read, write} to common library
    We are going to need these functions to implement support for Broadcom
    Wirespeed, aka downshift.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    drivers/net/phy/bcm-phy-lib.h

--- drivers/net/phy/bcm-phy-lib.h
+++ drivers/net/phy/bcm-phy-lib.h
@@ -26,6 +26,9 @@ static inline int bcm_phy_write_exp_sel(struct phy_device *phydev,
 	return bcm_phy_write_exp(phydev, reg | MII_BCM54XX_EXP_SEL_ER, val);
 }
 
+int bcm54xx_auxctl_write(struct phy_device *phydev, u16 regnum, u16 val);
+int bcm54xx_auxctl_read(struct phy_device *phydev, u16 regnum);
+
 int bcm_phy_write_misc(struct phy_device *phydev,
 		       u16 reg, u16 chl, u16 value);
 int bcm_phy_read_misc(struct phy_device *phydev,
