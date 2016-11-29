f0ec1a8102a4cf9ed4f6cc5c2681763aa58a8865
John Stultz <john.stultz@linaro.org>
2016-11-28 17:22:31 -0800

    drm/bridge: adv7511: Add Audio support
    This patch adds support to Audio for both adv7511 and adv7533
    bridge chips.
    
    This patch was originally from [1] by Lars-Peter Clausen <lars@metafoo.de>
    and was adapted by Archit Taneja <architt@codeaurora.org> and
    Srinivas Kandagatla <srinivas.kandagatla@linaro.org>.
    
    Then I heavily reworked it to use the hdmi-codec driver. And also
    folded in some audio packet initialization done by Andy Green
    <andy.green@linaro.org>. So credit to them, but blame to me.
    
    [1] https://github.com/analogdevicesinc/linux/blob/xcomm_zynq/drivers/gpu/drm/i2c/adv7511_audio.c
    
    Cc: David Airlie <airlied@linux.ie>
    Cc: Archit Taneja <architt@codeaurora.org>
    Cc: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Cc: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Cc: "Ville Syrjälä" <ville.syrjala@linux.intel.com>
    Cc: Boris Brezillon <boris.brezillon@free-electrons.com>
    Cc: Andy Green <andy@warmcat.com>
    Cc: Dave Long <dave.long@linaro.org>
    Cc: Guodong Xu <guodong.xu@linaro.org>
    Cc: Zhangfei Gao <zhangfei.gao@linaro.org>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Lars-Peter Clausen <lars@metafoo.de>
    Cc: Jose Abreu <joabreu@synopsys.com>
    Cc: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Cc: dri-devel@lists.freedesktop.org
    Acked-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Archit Taneja <architt@codeaurora.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/1480382552-28219-2-git-send-email-john.stultz@linaro.org

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -1076,6 +1076,8 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 		goto err_unregister_cec;
 	}
 
+	adv7511_audio_init(dev, adv7511);
+
 	return 0;
 
 err_unregister_cec:
@@ -1096,6 +1098,8 @@ static int adv7511_remove(struct i2c_client *i2c)
 	}
 
 	drm_bridge_remove(&adv7511->bridge);
+
+	adv7511_audio_exit(adv7511);
 
 	i2c_unregister_device(adv7511->i2c_edid);
 
