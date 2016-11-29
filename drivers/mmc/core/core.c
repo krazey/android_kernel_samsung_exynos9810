b5b8a850c150cb1cdfaeb8047a2e6b954d972e03
Adrian Hunter <adrian.hunter@intel.com>
2016-11-29 12:09:10 +0200

    mmc: queue: Fix queue thread wake-up
    The only time the driver sleeps expecting to be woken upon the arrival of
    a new request, is when the dispatch queue is empty. The only time that it
    is known whether the dispatch queue is empty is after NULL is returned
    from blk_fetch_request() while under the queue lock.
    
    Recognizing those facts, simplify the synchronization between the queue
    thread and the request function. A couple of flags tell the request
    function what to do, and the queue lock and barriers associated with
    wake-ups ensure synchronization.
    
    The result is simpler and allows the removal of the context_info lock.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Harjani Ritesh <riteshh@codeaurora.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    
    Conflicts:
    drivers/mmc/card/queue.c
    drivers/mmc/core/core.c
    
    Signed-off-by: krazey <admin@krazey.de>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -432,12 +432,9 @@ EXPORT_SYMBOL(mmc_start_bkops);
 static void mmc_wait_data_done(struct mmc_request *mrq)
 {
 	struct mmc_context_info *context_info = &mrq->host->context_info;
-	unsigned long flags;
 
-	spin_lock_irqsave(&mrq->host->context_info.lock, flags);
 	context_info->is_done_rcv = true;
 	wake_up_interruptible(&context_info->wait);
-	spin_unlock_irqrestore(&mrq->host->context_info.lock, flags);
 }
 
 static void mmc_wait_done(struct mmc_request *mrq)
@@ -527,18 +524,14 @@ static enum mmc_blk_status mmc_wait_for_data_req_done(struct mmc_host *host,
 	struct mmc_command *cmd;
 	struct mmc_context_info *context_info = &host->context_info;
 	enum mmc_blk_status status;
-	unsigned long flags;
 
 	while (1) {
 		wait_event_interruptible(context_info->wait,
 				(context_info->is_done_rcv ||
 				 context_info->is_new_req));
-		spin_lock_irqsave(&context_info->lock, flags);
 		context_info->is_waiting_last_req = false;
 		if (context_info->is_done_rcv) {
 			context_info->is_done_rcv = false;
-			context_info->is_new_req = false;
-			spin_unlock_irqrestore(&context_info->lock, flags);
 			cmd = mrq->cmd;
 
 			if (!cmd->error || !cmd->retries ||
@@ -557,15 +550,11 @@ static enum mmc_blk_status mmc_wait_for_data_req_done(struct mmc_host *host,
 				continue; /* wait for done/new event again */
 			}
 		} else if (context_info->is_new_req) {
-			context_info->is_new_req = false;
 			if (!next_req) {
-				spin_unlock_irqrestore(&context_info->lock,
-							flags);
 				status = MMC_BLK_NEW_REQUEST;
 				break; /* return err */
 			}
 		}
-		spin_unlock_irqrestore(&context_info->lock, flags);
 	} /* while */
 	if (status != MMC_BLK_NEW_REQUEST)
 		mmc_retune_release(host);
@@ -3170,7 +3159,6 @@ void mmc_unregister_pm_notifier(struct mmc_host *host)
  */
 void mmc_init_context_info(struct mmc_host *host)
 {
-	spin_lock_init(&host->context_info.lock);
 	host->context_info.is_new_req = false;
 	host->context_info.is_done_rcv = false;
 	host->context_info.is_waiting_last_req = false;
