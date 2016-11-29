416fe446358fe5bf53eacb0af5a4c00da3f6ceb9
Raju Lakkaraju <Raju.Lakkaraju@microsemi.com>
2016-11-29 15:16:49 +0530

    net: phy: Fix the mdix_ctrl changes
    PHY drivers to have an eth_tp_mdix_ctrl to indicate what is the configured
    MDI setting, and read eth_tp_mdi to indicate what is the current status,
    
    Add new parameter mdix_ctrl in phy_device structure and fix driver.
    
    Signed-off-by: Raju Lakkaraju <Raju.Lakkaraju@microsemi.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    drivers/net/phy/marvell.c

--- drivers/net/phy/marvell.c
+++ drivers/net/phy/marvell.c
@@ -240,7 +240,35 @@ static int marvell_config_aneg(struct phy_device *phydev)
 {
 	int err;
 
-	err = marvell_set_polarity(phydev, phydev->mdix);
+	/* The Marvell PHY has an errata which requires
+	 * that certain registers get written in order
+	 * to restart autonegotiation */
+	err = phy_write(phydev, MII_BMCR, BMCR_RESET);
+
+	if (err < 0)
+		return err;
+
+	err = phy_write(phydev, 0x1d, 0x1f);
+	if (err < 0)
+		return err;
+
+	err = phy_write(phydev, 0x1e, 0x200c);
+	if (err < 0)
+		return err;
+
+	err = phy_write(phydev, 0x1d, 0x5);
+	if (err < 0)
+		return err;
+
+	err = phy_write(phydev, 0x1e, 0);
+	if (err < 0)
+		return err;
+
+	err = phy_write(phydev, 0x1e, 0x100);
+	if (err < 0)
+		return err;
+
+	err = marvell_set_polarity(phydev, phydev->mdix_ctrl);
 	if (err < 0)
 		return err;
 
@@ -319,7 +347,7 @@ static int m88e1111_config_aneg(struct phy_device *phydev)
 	 */
 	err = phy_write(phydev, MII_BMCR, BMCR_RESET);
 
-	err = marvell_set_polarity(phydev, phydev->mdix);
+	err = marvell_set_polarity(phydev, phydev->mdix_ctrl);
 	if (err < 0)
 		return err;
 
