9b7c0aa1f39ec768a2d65592642c7a5ce0668ab6
Gao Feng <fgao@ikuai8.com>
2016-12-07 08:44:47 +0800

    driver: ipvlan: Free ipvl_port directly with kfree instead of kfree_rcu
    There are two functions which would free the ipvl_port now. The first
    is ipvlan_port_create. It frees the ipvl_port in the error handler,
    so it could kfree it directly. The second is ipvlan_port_destroy. It
    invokes netdev_rx_handler_unregister which enforces one grace period
    by synchronize_net firstly, so it also could kfree the ipvl_port
    directly and safely.
    
    So it is unnecessary to use kfree_rcu to free ipvl_port.
    
    Signed-off-by: Gao Feng <fgao@ikuai8.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ipvlan/ipvlan_main.c
+++ drivers/net/ipvlan/ipvlan_main.c
@@ -148,7 +148,7 @@ static int ipvlan_port_create(struct net_device *dev)
 	return 0;
 
 err:
-	kfree_rcu(port, rcu);
+	kfree(port);
 	return err;
 }
 
@@ -165,7 +165,7 @@ static void ipvlan_port_destroy(struct net_device *dev)
 	netdev_rx_handler_unregister(dev);
 	cancel_work_sync(&port->wq);
 	__skb_queue_purge(&port->backlog);
-	kfree_rcu(port, rcu);
+	kfree(port);
 }
 
 #define IPVLAN_ALWAYS_ON_OFLOADS \
