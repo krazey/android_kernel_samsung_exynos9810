3388c7a2502b29e0632652d5ac772a7ee8f17635
xing wei <xing.wei@intel.com>
2016-12-07 19:31:16 +0800

    PM / sleep: Print active wakeup sources when blocking on wakeup_count reads
    If there are any wakeup events being processed, read operation
    on /sys/power/wakeup_count will be blocked, so print the names
    of all active wakeup sources to help to find out who is preventing
    system suspend from triggering.
    
    While at it change pr_info() in pm_print_active_wakeup_sources()
    to pr_debug() to avoid excessive log noise.
    
    Signed-off-by: xing wei <xing.wei@intel.com>
    [ rjw: Subject & changelog ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -934,7 +934,7 @@ void pm_print_active_wakeup_sources(void)
 	srcuidx = srcu_read_lock(&wakeup_srcu);
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
 		if (ws->active) {
-			pr_info("active wakeup source: %s\n", ws->name);
+			pr_debug("active wakeup source: %s\n", ws->name);
 			active = 1;
 		} else if (!active &&
 			   (!last_activity_ws ||
@@ -945,7 +945,7 @@ void pm_print_active_wakeup_sources(void)
 	}
 
 	if (!active && last_activity_ws)
-		pr_info("last active wakeup source: %s\n",
+		pr_debug("last active wakeup source: %s\n",
 			last_activity_ws->name);
 	srcu_read_unlock(&wakeup_srcu, srcuidx);
 }
@@ -1049,7 +1049,7 @@ bool pm_get_wakeup_count(unsigned int *count, bool block)
 			split_counters(&cnt, &inpr);
 			if (inpr == 0 || signal_pending(current))
 				break;
-
+			pm_print_active_wakeup_sources();
 			schedule();
 		}
 		finish_wait(&wakeup_count_wait_queue, &wait);
