de71ff1b3bbadf54ea13dd3f966b601282fdd6a5
Joseph Lo <josephl@nvidia.com>
2016-12-16 18:57:36 +0100

    mfd: cros_ec: Prevent data transfer while device is suspended
    The cros_ec driver is still active while the device is suspended.
    Besides that, it also tries to transfer data even after the I2C host had
    been suspended. This patch uses a simple flag to prevent this.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Thierry Escande <thierry.escande@collabora.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/cros_ec.c
+++ drivers/mfd/cros_ec.c
@@ -169,6 +169,7 @@ int cros_ec_suspend(struct cros_ec_device *ec_dev)
 
 	disable_irq(ec_dev->irq);
 	ec_dev->was_wake_device = ec_dev->wake_enabled;
+	ec_dev->suspended = true;
 
 	return 0;
 }
@@ -183,6 +184,7 @@ static void cros_ec_drain_events(struct cros_ec_device *ec_dev)
 
 int cros_ec_resume(struct cros_ec_device *ec_dev)
 {
+	ec_dev->suspended = false;
 	enable_irq(ec_dev->irq);
 
 	/*
