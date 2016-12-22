74f4488c044d09f48a6d6ad4f94b944f8ae07a8d
Andrew Boyer <andrew.boyer@dell.com>
2016-12-22 08:54:38 -0500

    IB/rxe: Don't check for null ptr in send()
    pkt->qp was already dereferenced earlier in the function.
    
    Fixes Smatch complaint:
    drivers/infiniband/sw/rxe/rxe_net.c:458 send()
    	 warn: variable dereferenced before check 'pkt->qp' (see line 441)
    
    Signed-off-by: Andrew Boyer <andrew.boyer@dell.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/sw/rxe/rxe_net.c
+++ drivers/infiniband/sw/rxe/rxe_net.c
@@ -454,8 +454,7 @@ int rxe_send(struct rxe_dev *rxe, struct rxe_pkt_info *pkt, struct sk_buff *skb)
 		return -EAGAIN;
 	}
 
-	if (pkt->qp)
-		atomic_inc(&pkt->qp->skb_out);
+	atomic_inc(&pkt->qp->skb_out);
 	kfree_skb(skb);
 
 	return 0;
