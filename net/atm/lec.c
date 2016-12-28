e8ebe7c1a9080c66918d4337d307605477bc3476
Augusto Mecking Caringi <augustocaringi@gmail.com>
2016-12-28 16:02:05 +0000

    net: atm: Fix warnings in net/atm/lec.c when !CONFIG_PROC_FS
    This patch fixes the following warnings when CONFIG_PROC_FS is not set:
    
    linux/net/atm/lec.c: In function ‘lane_module_cleanup’:
    linux/net/atm/lec.c:1062:27: error: ‘atm_proc_root’ undeclared (first
    use in this function)
    remove_proc_entry("lec", atm_proc_root);
                               ^
    linux/net/atm/lec.c:1062:27: note: each undeclared identifier is
    reported only once for each function it appears in
    
    Signed-off-by: Augusto Mecking Caringi <augustocaringi@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/atm/lec.c
+++ net/atm/lec.c
@@ -1068,7 +1068,9 @@ static void __exit lane_module_cleanup(void)
 {
 	int i;
 
+#ifdef CONFIG_PROC_FS
 	remove_proc_entry("lec", atm_proc_root);
+#endif
 
 	deregister_atm_ioctl(&lane_ioctl_ops);
 
