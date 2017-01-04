f9afc37e8f66866449f145bb21c12a0ca6c28681
Chris Wilson <chris@chris-wilson.co.uk>
2017-01-04 14:51:10 +0000

    drm/i915: Revoke fenced GTT mmapings across GPU reset
    The fence registers are clobbered by a GPU reset. If there is concurrent
    user access to a fenced region via a GTT mmaping, the access will not be
    fenced during the reset (until we restore the fences afterwards). In order
    to prevent invalid access during the reset, before we clobber the fences
    first we must invalidate the GTT mmapings. Access to the mmap will then
    be forced to fault in the page, and in handling the fault, i915_gem_fault()
    will take the struct_mutex and wait upon the reset to complete.
    
    v2: Fix up commentary.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=99274
    Testcase: igt/gem_mmap_gtt/hang
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170104145110.1486-1-chris@chris-wilson.co.uk
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -3340,7 +3340,8 @@ static inline u32 i915_reset_count(struct i915_gpu_error *error)
 	return READ_ONCE(error->reset_count);
 }
 
-void i915_gem_reset(struct drm_i915_private *dev_priv);
+void i915_gem_reset_prepare(struct drm_i915_private *dev_priv);
+void i915_gem_reset_finish(struct drm_i915_private *dev_priv);
 void i915_gem_set_wedged(struct drm_i915_private *dev_priv);
 void i915_gem_clflush_object(struct drm_i915_gem_object *obj, bool force);
 int __must_check i915_gem_init(struct drm_i915_private *dev_priv);
@@ -3417,6 +3418,7 @@ i915_gem_object_to_ggtt(struct drm_i915_gem_object *obj,
 int __must_check i915_vma_get_fence(struct i915_vma *vma);
 int __must_check i915_vma_put_fence(struct i915_vma *vma);
 
+void i915_gem_revoke_fences(struct drm_i915_private *dev_priv);
 void i915_gem_restore_fences(struct drm_i915_private *dev_priv);
 
 void i915_gem_detect_bit_6_swizzle(struct drm_i915_private *dev_priv);
