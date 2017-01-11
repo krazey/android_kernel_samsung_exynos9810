dbdbbe22b148415d6b85cc49167c06154a50e423
Timur Tabi <timur@codeaurora.org>
2017-01-11 16:45:51 -0600

    net: qcom/emac: grab a reference to the phydev on ACPI systems
    Commit 6ffe1c4cd0a7 ("net: qcom/emac: fix of_node and phydev leaks")
    fixed the problem with reference leaks on phydev, but the fix is
    device-tree specific.  When the driver unloads, the reference is
    dropped only on DT systems.
    
    Instead, it's cleaner if up grab an reference on ACPI systems.
    When the driver unloads, we can drop the reference without having
    to check whether we're on a DT system.
    
    Signed-off-by: Timur Tabi <timur@codeaurora.org>
    Reviewed-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qualcomm/emac/emac.c
+++ drivers/net/ethernet/qualcomm/emac/emac.c
@@ -730,8 +730,7 @@ static int emac_probe(struct platform_device *pdev)
 err_undo_napi:
 	netif_napi_del(&adpt->rx_q.napi);
 err_undo_mdiobus:
-	if (!has_acpi_companion(&pdev->dev))
-		put_device(&adpt->phydev->mdio.dev);
+	put_device(&adpt->phydev->mdio.dev);
 	mdiobus_unregister(adpt->mii_bus);
 err_undo_clocks:
 	emac_clks_teardown(adpt);
@@ -751,8 +750,7 @@ static int emac_remove(struct platform_device *pdev)
 
 	emac_clks_teardown(adpt);
 
-	if (!has_acpi_companion(&pdev->dev))
-		put_device(&adpt->phydev->mdio.dev);
+	put_device(&adpt->phydev->mdio.dev);
 	mdiobus_unregister(adpt->mii_bus);
 
 	if (adpt->phy.digital)
