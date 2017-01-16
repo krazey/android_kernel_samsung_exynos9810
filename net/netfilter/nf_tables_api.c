d6cf11b6a8838968da8d9a99446662d8b2e7c246
Gao Feng <fgao@ikuai8.com>
2017-01-16 22:02:57 +0800

    netfilter: nf_tables: eliminate useless condition checks
    The return value of nf_tables_table_lookup() is valid pointer or one
    pointer error. There are two cases:
    
    1) IS_ERR(table) is true, it would return the error or reset the
       table as NULL, it is unnecessary to perform the latter check
       "table != NULL".
    
    2) IS_ERR(obj) is false, the table is one valid pointer. It is also
       unnecessary to perform that check.
    
    The nf_tables_newset() and nf_tables_newobj() have same logic codes.
    
    In summary, we could move the block of condition check "table != NULL"
    in the else block to eliminate the original condition checks.
    
    Signed-off-by: Gao Feng <fgao@ikuai8.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -699,10 +699,7 @@ static int nf_tables_newtable(struct net *net, struct sock *nlsk,
 	if (IS_ERR(table)) {
 		if (PTR_ERR(table) != -ENOENT)
 			return PTR_ERR(table);
-		table = NULL;
-	}
-
-	if (table != NULL) {
+	} else {
 		if (nlh->nlmsg_flags & NLM_F_EXCL)
 			return -EEXIST;
 		if (nlh->nlmsg_flags & NLM_F_REPLACE)
@@ -2976,10 +2973,7 @@ static int nf_tables_newset(struct net *net, struct sock *nlsk,
 	if (IS_ERR(set)) {
 		if (PTR_ERR(set) != -ENOENT)
 			return PTR_ERR(set);
-		set = NULL;
-	}
-
-	if (set != NULL) {
+	} else {
 		if (nlh->nlmsg_flags & NLM_F_EXCL)
 			return -EEXIST;
 		if (nlh->nlmsg_flags & NLM_F_REPLACE)
@@ -4173,10 +4167,7 @@ static int nf_tables_newobj(struct net *net, struct sock *nlsk,
 		if (err != -ENOENT)
 			return err;
 
-		obj = NULL;
-	}
-
-	if (obj != NULL) {
+	} else {
 		if (nlh->nlmsg_flags & NLM_F_EXCL)
 			return -EEXIST;
 
