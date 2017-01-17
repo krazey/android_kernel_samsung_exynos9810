03af95874b12a594cb1791d954ea6a026fbfe480
Xin Long <lucien.xin@gmail.com>
2017-01-18 00:44:46 +0800

    sctp: add sockopt SCTP_ENABLE_STREAM_RESET
    This patch is to add sockopt SCTP_ENABLE_STREAM_RESET to get/set
    strreset_enable to indicate which reconf request type it supports,
    which is described in rfc6525 section 6.3.1.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sctp/structs.h
+++ include/net/sctp/structs.h
@@ -1260,6 +1260,8 @@ struct sctp_endpoint {
 	__u8  auth_enable:1,
 	      prsctp_enable:1,
 	      reconf_enable:1;
+
+	__u8  strreset_enable;
 };
 
 /* Recover the outter endpoint structure. */
@@ -1874,6 +1876,8 @@ struct sctp_association {
 	     temp:1,		/* Is it a temporary association? */
 	     prsctp_enable:1,
 	     reconf_enable:1;
+
+	__u8 strreset_enable;
 
 	__u32 strreset_outseq; /* Update after receiving response */
 	__u32 strreset_inseq; /* Update after receiving request */
