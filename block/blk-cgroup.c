820bcb28738ce8a8ee60ad687d63a5ed39e68b28
Jens Axboe <axboe@fb.com>
2017-01-18 07:43:26 -0700

    blk-cgroup: ensure that we clear the stop bit on quiesced queues
    If we call blk_mq_quiesce_queue() on a queue, we must remember to
    pair that with something that clears the stopped by on the
    queues later on.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -1263,9 +1263,10 @@ pd_prealloc:
 
 	spin_unlock_irq(q->queue_lock);
 out_bypass_end:
-	if (q->mq_ops)
+	if (q->mq_ops) {
 		blk_mq_unfreeze_queue(q);
-	else
+		blk_mq_start_stopped_hw_queues(q, true);
+	} else
 		blk_queue_bypass_end(q);
 	if (pd_prealloc)
 		pol->pd_free_fn(pd_prealloc);
@@ -1315,9 +1316,10 @@ void blkcg_deactivate_policy(struct request_queue *q,
 
 	spin_unlock_irq(q->queue_lock);
 
-	if (q->mq_ops)
+	if (q->mq_ops) {
 		blk_mq_unfreeze_queue(q);
-	else
+		blk_mq_start_stopped_hw_queues(q, true);
+	} else
 		blk_queue_bypass_end(q);
 }
 EXPORT_SYMBOL_GPL(blkcg_deactivate_policy);
