b3cc425b30cd597a32bad3dcd24db85da0db13fd
Markus Elfring <elfring@users.sourceforge.net>
2017-01-21 15:30:15 +0100

    powerpc/sstep: Return directly after a failed address_ok() in emulate_step()
    Setting err and going to ldst_done just returns 0, without using err, so
    just return 0 directly. We already do that for other call sites in this
    function.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    [mpe: Rewrite change log]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/lib/sstep.c
+++ arch/powerpc/lib/sstep.c
@@ -1825,9 +1825,8 @@ int __kprobes emulate_step(struct pt_regs *regs, unsigned int instr)
 	case LARX:
 		if (op.ea & (size - 1))
 			break;		/* can't handle misaligned */
-		err = -EFAULT;
 		if (!address_ok(regs, op.ea, size))
-			goto ldst_done;
+			return 0;
 		err = 0;
 		switch (size) {
 		case 4:
@@ -1848,9 +1847,8 @@ int __kprobes emulate_step(struct pt_regs *regs, unsigned int instr)
 	case STCX:
 		if (op.ea & (size - 1))
 			break;		/* can't handle misaligned */
-		err = -EFAULT;
 		if (!address_ok(regs, op.ea, size))
-			goto ldst_done;
+			return 0;
 		err = 0;
 		switch (size) {
 		case 4:
