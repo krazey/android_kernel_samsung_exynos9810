9bb2ac26c5224a94e300845910c35d2ad15bc886
Christoph Hellwig <hch@lst.de>
2017-01-27 09:35:54 +0100

    block: cleanup tracing
    A couple tweaks to the tracing code:
    
     - trace the request size for all requests
     - trace request sector and nr_sectors only for fs requests, enforced by
       helpers
     - drop SCSI CDB tracing - we have SCSI tracing for this and are going
       to me the CDB out of the generic struct request soon.
    
    With this the tracing code stops to know about BLOCK_PC requests entirely,
    it's just FS vs passthrough requests now, where the latter includes any
    driver-private requests.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    kernel/trace/blktrace.c

--- kernel/trace/blktrace.c
+++ kernel/trace/blktrace.c
@@ -791,15 +791,14 @@ static void blk_add_trace_rq(struct request_queue *q, struct request *rq,
 		return;
 	}
 
-	if (rq->cmd_type == REQ_TYPE_BLOCK_PC) {
+	if (rq->cmd_type != REQ_TYPE_FS)
 		what |= BLK_TC_ACT(BLK_TC_PC);
-		__blk_add_trace(bt, 0, nr_bytes, req_op(rq), rq->cmd_flags,
-				what, rq->errors, rq->cmd_len, rq->cmd);
-	} else  {
+	else
 		what |= BLK_TC_ACT(BLK_TC_FS);
-		__blk_add_trace(bt, blk_rq_pos(rq), nr_bytes, req_op(rq),
-				rq->cmd_flags, what, rq->errors, 0, NULL);
-	}
+
+	__blk_add_trace(bt, blk_rq_trace_sector(rq), nr_bytes, req_op(rq),
+			rq->cmd_flags, what, rq->errors, 0, NULL);
+
 	rcu_read_unlock();
 }
 
@@ -1086,11 +1085,7 @@ void blk_add_driver_data(struct request_queue *q,
 		return;
 	}
 
-	if (rq->cmd_type == REQ_TYPE_BLOCK_PC)
-		__blk_add_trace(bt, 0, blk_rq_bytes(rq), 0, 0,
-				BLK_TA_DRV_DATA, rq->errors, len, data);
-	else
-		__blk_add_trace(bt, blk_rq_pos(rq), blk_rq_bytes(rq), 0, 0,
+	__blk_add_trace(bt, blk_rq_trace_sector(rq), blk_rq_bytes(rq), 0, 0,
 				BLK_TA_DRV_DATA, rq->errors, len, data);
 	rcu_read_unlock();
 }
@@ -1885,31 +1880,6 @@ void blk_trace_remove_sysfs(struct device *dev)
 #endif /* CONFIG_BLK_DEV_IO_TRACE */
 
 #ifdef CONFIG_EVENT_TRACING
-
-void blk_dump_cmd(char *buf, struct request *rq)
-{
-	int i, end;
-	int len = rq->cmd_len;
-	unsigned char *cmd = rq->cmd;
-
-	if (rq->cmd_type != REQ_TYPE_BLOCK_PC) {
-		buf[0] = '\0';
-		return;
-	}
-
-	for (end = len - 1; end >= 0; end--)
-		if (cmd[end])
-			break;
-	end++;
-
-	for (i = 0; i < len; i++) {
-		buf += sprintf(buf, "%s%02x", i == 0 ? "" : " ", cmd[i]);
-		if (i == end && end != len - 1) {
-			sprintf(buf, " ..");
-			break;
-		}
-	}
-}
 
 SIO_PATCH_VERSION(ftrace_discard_bugfix, 1, 0, "");
 
