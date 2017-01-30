84f4bf1db106895854b08b8c2be3cd921cd47264
Ping Cheng <pinglinux@gmail.com>
2017-01-30 15:40:49 -0800

    HID: wacom: don't apply generic settings to old devices
    Non-generic devices have numbered_buttons set for both pen and
    touch interfaces by default. The actual number of buttons on the
    interface is normally manually decided later, which is different
    from what those HID generic devices are processed, where number
    of buttons are directly retrieved from HID descriptors.
    
    This patch adds the missed HID_GENERIC check and moves the statement
    to wacom_setup_pad_input_capabilities since it's not a quirk anymore.
    
    Signed-off-by: Ping Cheng <ping.cheng@wacom.com>
    Reviewed-by: Jason Gerecke <jason.gerecke@wacom.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/wacom_wac.c
+++ drivers/hid/wacom_wac.c
@@ -2992,8 +2992,6 @@ void wacom_setup_device_quirks(struct wacom *wacom)
 	struct wacom_features *features = &wacom->wacom_wac.features;
 
 	/* The pen and pad share the same interface on most devices */
-	if (features->numbered_buttons > 0)
-		features->device_type |= WACOM_DEVICETYPE_PAD;
 	if (features->type == GRAPHIRE_BT || features->type == WACOM_G4 ||
 	    features->type == DTUS ||
 	    (features->type >= INTUOS3S && features->type <= WACOM_MO)) {
