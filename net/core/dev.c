78f58db854e4ffe01c01d4354b86b96a482f0f6d
Steffen Klassert <steffen.klassert@secunet.com>
2017-01-30 06:45:38 +0100

    net: Drop secpath on free after gro merge.
    With a followup patch, a gro merged skb can have a secpath.
    So drop it before freeing or reusing the skb.
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    net/core/dev.c

--- net/core/dev.c
+++ net/core/dev.c
@@ -4675,10 +4675,12 @@ static gro_result_t napi_skb_finish(gro_result_t ret, struct sk_buff *skb)
 		break;
 
 	case GRO_MERGED_FREE:
-		if (NAPI_GRO_CB(skb)->free == NAPI_GRO_FREE_STOLEN_HEAD)
+		if (NAPI_GRO_CB(skb)->free == NAPI_GRO_FREE_STOLEN_HEAD) {
+			secpath_reset(skb);
 			napi_skb_free_stolen_head(skb);
-		else
+		} else {
 			__kfree_skb(skb);
+		}
 		break;
 
 	case GRO_HELD:
@@ -4719,6 +4721,7 @@ static void napi_reuse_skb(struct napi_struct *napi, struct sk_buff *skb)
 	skb->encapsulation = 0;
 	skb_shinfo(skb)->gso_type = 0;
 	skb->truesize = SKB_TRUESIZE(skb_end_offset(skb));
+	secpath_reset(skb);
 
 	napi->skb = skb;
 }
