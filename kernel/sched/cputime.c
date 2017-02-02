b2a5d592e65d7d626cf33d68c5ed35f846af47ef
Ingo Molnar <mingo@kernel.org>
2017-02-02 14:47:27 +0100

    sched/headers: Prepare header dependency changes, move the <asm/paravirt.h> include to kernel/sched/sched.h
    Recent header reorganizations unearthed this hidden dependency:
    
      kernel/sched/core.c:199:25: error: 'paravirt_steal_rq_enabled' undeclared (first use in this function)
      kernel/sched/core.c:200:11: error: implicit declaration of function 'paravirt_steal_clock' [-Werror=implicit-function-declaration]
    
    So move the asm/paravirt.h include from kernel/sched/cpuclock.c to kernel/sched/sched.h.
    
    ( NOTE: We do this change before doing the changes that introduce the build failure,
            so the series remains fully bisectable. )
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    kernel/sched/cputime.c

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -7,9 +7,6 @@
 #include <linux/cpufreq_times.h>
 #include <linux/cputime.h>
 #include "sched.h"
-#ifdef CONFIG_PARAVIRT
-#include <asm/paravirt.h>
-#endif
 #include "walt.h"
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
