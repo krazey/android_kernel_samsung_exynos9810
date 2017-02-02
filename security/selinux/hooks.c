0a47fab53b440e6defbbe67115bf3581981a758e
Antonio Murdaca <amurdaca@redhat.com>
2017-02-02 16:22:57 +0100

    selinux: allow changing labels for cgroupfs
    This patch allows changing labels for cgroup mounts. Previously, running
    chcon on cgroupfs would throw an "Operation not supported". This patch
    specifically whitelist cgroupfs.
    
    The patch could also allow containers to write only to the systemd cgroup
    for instance, while the other cgroups are kept with cgroup_t label.
    
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    security/selinux/hooks.c

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -592,6 +592,8 @@ static int selinux_is_genfs_special_handling(struct super_block *sb)
 {
 	/* Special handling. Genfs but also in-core setxattr handler */
 	return	!strcmp(sb->s_type->name, "sysfs") ||
+		!strcmp(sb->s_type->name, "cgroup") ||
+		!strcmp(sb->s_type->name, "cgroup2") ||
 		!strcmp(sb->s_type->name, "pstore") ||
 		!strcmp(sb->s_type->name, "debugfs") ||
 		!strcmp(sb->s_type->name, "tracefs") ||
