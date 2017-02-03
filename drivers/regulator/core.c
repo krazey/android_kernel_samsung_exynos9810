885bc9f3d7490e11809f9c828f3cdcbe5be9e74f
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2017-02-03 13:56:00 -0800

    regulator: core: remove dead code in _regulator_get()
    There is no point in assigning value to 'ret' before calling
    regulator_dev_lookup() as it will clobber 'ret' anyway.
    
    Also, let's explicitly return -PROBE_DEFER when try_module_get() fails,
    instead of relying that earlier initialization of "regulator" carries
    correct value.
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1604,7 +1604,7 @@ static struct regulator *_regulator_get(struct device *dev, const char *id,
 					bool exclusive, bool allow_dummy)
 {
 	struct regulator_dev *rdev;
-	struct regulator *regulator = ERR_PTR(-EPROBE_DEFER);
+	struct regulator *regulator;
 	const char *devname = NULL;
 	int ret;
 
@@ -1615,11 +1615,6 @@ static struct regulator *_regulator_get(struct device *dev, const char *id,
 
 	if (dev)
 		devname = dev_name(dev);
-
-	if (have_full_constraints())
-		ret = -ENODEV;
-	else
-		ret = -EPROBE_DEFER;
 
 	rdev = regulator_dev_lookup(dev, id, &ret);
 	if (rdev)
@@ -1676,6 +1671,7 @@ found:
 	}
 
 	if (!try_module_get(rdev->owner)) {
+		regulator = ERR_PTR(-EPROBE_DEFER);
 		put_device(&rdev->dev);
 		return regulator;
 	}
