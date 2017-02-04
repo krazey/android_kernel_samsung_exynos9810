1baa5c5bf0b5fcc403ae7cb4b5f47bc862722bf1
Ingo Molnar <mingo@kernel.org>
2017-02-04 01:20:53 +0100

    sched/headers: Prepare to move 'init_task' and 'init_thread_union' from <linux/sched.h> to <linux/sched/task.h>
    Update all usage sites first.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    fs/namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -25,6 +25,8 @@
 #include <linux/magic.h>
 #include <linux/bootmem.h>
 #include <linux/task_work.h>
+#include <linux/sched/task.h>
+
 #include <linux/slub_def.h>
 #include <linux/fslog.h>
 #include "pnode.h"
