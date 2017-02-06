6710e1c34f7c907836db1e77157de0279d51dbbf
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-02-06 15:02:42 +0000

    arm64: Revert "arm64: kaslr: fix breakage with CONFIG_MODVERSIONS=y"
    This reverts commit 9c0e83c371cf4696926c95f9c8c77cd6ea803426, which
    is no longer needed now that the modversions code plays nice with
    relocatable PIE kernels.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    arch/arm64/include/asm/module.h

--- arch/arm64/include/asm/module.h
+++ arch/arm64/include/asm/module.h
@@ -17,7 +17,6 @@
 #define __ASM_MODULE_H
 
 #include <asm-generic/module.h>
-#include <asm/memory.h>
 
 #define MODULE_ARCH_VERMAGIC	"aarch64"
 
@@ -32,12 +31,6 @@ struct mod_arch_specific {
 u64 module_emit_plt_entry(struct module *mod, const Elf64_Rela *rela,
 			  Elf64_Sym *sym);
 
-#if defined(CONFIG_RANDOMIZE_BASE) || defined(CONFIG_RELOCATABLE_KERNEL)
-#ifdef CONFIG_MODVERSIONS
-#define ARCH_RELOCATES_KCRCTAB
-#define reloc_start 		(kimage_vaddr - KIMAGE_VADDR)
-#endif
-#endif
 #if defined(CONFIG_RANDOMIZE_BASE)
 extern u64 module_alloc_base;
 #else
