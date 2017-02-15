e128f38e75082b43fac98dfcd5992e9a67d110b8
Chris Wilson <chris@chris-wilson.co.uk>
2017-02-15 08:43:40 +0000

    drm/i915: Convert clflushed pagetables over to WC maps
    We flush the entire page every time we update a few bytes, making the
    update of a page table many, many times slower than is required. If we
    create a WC map of the page for our updates, we can avoid the clflush
    but incur additional cost for creating the pagetable. We amoritize that
    cost by reusing page vmappings, and only changing the page protection in
    batches.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    drivers/gpu/drm/i915/i915_drv.h
    drivers/gpu/drm/i915/i915_gem_gtt.h

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2481,8 +2481,6 @@ struct drm_i915_private {
 	/* Used to save the pipe-to-encoder mapping for audio */
 	struct intel_encoder *av_enc_map[I915_MAX_PIPES];
 
-	I915_SELFTEST_DECLARE(struct fault_attr vm_fault);
-
 	/* necessary resource sharing with HDMI LPE audio driver. */
 	struct {
 		struct platform_device *platdev;
