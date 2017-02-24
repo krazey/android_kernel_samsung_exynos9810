50756c510ff2b8627715f917c0696fa4bbbdfd11
David Rientjes <rientjes@google.com>
2017-02-24 14:55:42 -0800

    mm, oom: header nodemask is NULL when cpusets are disabled
    Commit 82e7d3abec86 ("oom: print nodemask in the oom report") implicitly
    sets the allocation nodemask to cpuset_current_mems_allowed when there
    is no effective mempolicy.  cpuset_current_mems_allowed is only
    effective when cpusets are enabled, which is also printed by
    dump_header(), so setting the nodemask to cpuset_current_mems_allowed is
    redundant and prevents debugging issues where ac->nodemask is not set
    properly in the page allocator.
    
    This provides better debugging output since
    cpuset_print_current_mems_allowed() is already provided.
    
    [rientjes@google.com: newline per Hillf]
      Link: http://lkml.kernel.org/r/alpine.DEB.2.10.1701200158300.88321@chino.kir.corp.google.com
    Link: http://lkml.kernel.org/r/alpine.DEB.2.10.1701191454470.2381@chino.kir.corp.google.com
    Signed-off-by: David Rientjes <rientjes@google.com>
    Suggested-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Hillf Danton <hillf.zj@alibaba-inc.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    mm/oom_kill.c

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -463,13 +463,15 @@ int unregister_oomdebug_notifier(struct notifier_block *nb)
 
 static void dump_header(struct oom_control *oc, struct task_struct *p)
 {
-	nodemask_t *nm = (oc->nodemask) ? oc->nodemask : &cpuset_current_mems_allowed;
 	int v = 0;
-
-	pr_warn("%s invoked oom-killer: gfp_mask=%#x(%pGg), nodemask=%*pbl, order=%d, oom_score_adj=%hd\n",
-		current->comm, oc->gfp_mask, &oc->gfp_mask,
-		nodemask_pr_args(nm), oc->order,
-		current->signal->oom_score_adj);
+	pr_warn("%s invoked oom-killer: gfp_mask=%#x(%pGg), nodemask=",
+		current->comm, oc->gfp_mask, &oc->gfp_mask);
+	if (oc->nodemask)
+		pr_cont("%*pbl", nodemask_pr_args(oc->nodemask));
+	else
+		pr_cont("(null)");
+	pr_cont(",  order=%d, oom_score_adj=%hd\n",
+		oc->order, current->signal->oom_score_adj);
 	if (!IS_ENABLED(CONFIG_COMPACTION) && oc->order)
 		pr_warn("COMPACTION is disabled!!!\n");
 
@@ -478,7 +480,7 @@ static void dump_header(struct oom_control *oc, struct task_struct *p)
 	if (oc->memcg)
 		mem_cgroup_print_oom_info(oc->memcg, p);
 	else
-		show_mem(SHOW_MEM_FILTER_NODES, nm);
+		show_mem(SHOW_MEM_FILTER_NODES, oc->nodemask);
 	if (sysctl_oom_dump_tasks)
 		dump_tasks(oc->memcg, oc->nodemask);
 
