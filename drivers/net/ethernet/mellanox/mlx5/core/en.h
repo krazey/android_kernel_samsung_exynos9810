0fbdf236542a9a7e16359d0fb4c5d78c57a3d46a
Saeed Mahameed <saeedm@mellanox.com>
2017-03-25 00:52:08 +0300

    net/mlx5e: Move XDP SQ instance into RQ
    To save many rq->channel->sq dereferences in fast-path.
    And rename it to xdpsq.
    
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Reviewed-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -481,7 +481,10 @@ struct mlx5e_rq {
 	u16                    rx_headroom;
 
 	struct mlx5e_rx_am     am; /* Adaptive Moderation */
+
+	/* XDP */
 	struct bpf_prog       *xdp_prog;
+	struct mlx5e_sq        xdpsq;
 
 	/* control */
 	struct mlx5_wq_ctrl    wq_ctrl;
@@ -501,7 +504,6 @@ enum channel_flags {
 struct mlx5e_channel {
 	/* data path */
 	struct mlx5e_rq            rq;
-	struct mlx5e_sq            xdp_sq;
 	struct mlx5e_sq            sq[MLX5E_MAX_NUM_TC];
 	struct mlx5e_sq            icosq;   /* internal control operations */
 	bool                       xdp;
