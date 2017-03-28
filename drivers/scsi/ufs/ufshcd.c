ef820d0f1f50059d1d4f7df98cceb30891f84dea
Tomohiro Kusumi <tkusumi@tuxera.com>
2017-03-28 16:49:25 +0300

    scsi: ufs: use existing macro CONTROLLER_ENABLE to test register bit
    Signed-off-by: Tomohiro Kusumi <tkusumi@tuxera.com>
    Reviewed-by: Subhash Jadavani <subhashj@codeaurora.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1219,7 +1219,8 @@ static void set_customized_upiu_flags(struct ufshcd_lrb *lrbp, u32 *upiu_flags)
  */
 static inline bool ufshcd_is_hba_active(struct ufs_hba *hba)
 {
-	return (ufshcd_readl(hba, REG_CONTROLLER_ENABLE) & 0x1) ? false : true;
+	return (ufshcd_readl(hba, REG_CONTROLLER_ENABLE) & CONTROLLER_ENABLE)
+		? false : true;
 }
 
 static const char *ufschd_uic_link_state_to_string(
