89f66aedf2adc05157d92b9b7b7339f0f660b883
Daeyeong Lee <daeyeong.lee@samsung.com>
2018-05-08 19:56:09 +0900

    sched: ems: ontime: Remove distinction between ontime and normal task.
    Change-Id: I343feff2d7db0d97d3813b570193f1ee8e3af93e
    Signed-off-by: Daeyeong Lee <daeyeong.lee@samsung.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1356,10 +1356,6 @@ struct sched_avg {
 	struct util_est			util_est;
 };
 
-#define NOT_ONTIME		1
-#define ONTIME_MIGRATING	2
-#define ONTIME			4
-
 #define ontime_of(p)			(&p->se.ontime)
 
 struct ontime_avg {
@@ -1371,7 +1367,7 @@ struct ontime_avg {
 
 struct ontime_entity {
 	struct ontime_avg avg;
-	int flags;
+	int migrating;
 	int cpu;
 };
 
