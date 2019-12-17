e3ed564998b64921cd354be71bc31a355a710384
Satya Tangirala <satyat@google.com>
2019-12-17 14:26:29 -0800

    BACKPORT: FROMLIST: Update Inline Encryption from v5 to v6 of patch series
    Changes v5 => v6:
     - Blk-crypto's kernel crypto API fallback is no longer restricted to
       8-byte DUNs. It's also now separately configurable from blk-crypto, and
       can be disabled entirely, while still allowing the kernel to use inline
       encryption hardware. Further, struct bio_crypt_ctx takes up less space,
       and no longer contains the information needed by the crypto API
       fallback - the fallback allocates the required memory when necessary.
     - Blk-crypto now supports all file content encryption modes supported by
       fscrypt.
     - Fixed bio merging logic in blk-merge.c
     - Fscrypt now supports inline encryption with the direct key policy, since
       blk-crypto now has support for larger DUNs.
     - Keyslot manager now uses a hashtable to lookup which keyslot contains
       any particular key (thanks Eric!)
     - Fscrypt support for inline encryption now handles filesystems with
       multiple underlying block devices (thanks Eric!)
     - Numerous cleanups
    
    Backport notes: In the time between the update from v5 to v6,
    "scsi: ufs: override auto suspend tunables for ufs" was merged in
    upstream, and as a result, UFSHCD_CAP_RPM_AUTOSUSPEND took up the
    7th bit in the ufs crypto caps - however, that patch has not been
    backported to 4.14 yet, so we manually change UFSHCD_CAP_CRYPTO to
    use the 8th bit (to reflect what's in v6 in android-mainline).
    
    Bug: 137270441
    Test: refer to I26376479ee38259b8c35732cb3a1d7e15f9b05a3
    Change-Id: I13e2e327e0b4784b394cb1e7cf32a04856d95f01
    Link: https://lore.kernel.org/linux-block/20191218145136.172774-1-satyat@google.com/
    Signed-off-by: Satya Tangirala <satyat@google.com>
    
    Conflicts:
    	block/bio.c
    	block/blk-merge.c
    	block/keyslot-manager.c: kvmalloc_array -> kmalloc_array
    	drivers/md/dm.c
    	drivers/scsi/ufs/ufshcd.h
    
    Signed-off-by: krazey <admin@krazey.de>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -202,9 +202,11 @@ struct ufshcd_lrb {
 	u8 lun; /* UPIU LUN id field is only 8-bit wide */
 	bool intr_cmd;
 	ktime_t issue_time_stamp;
+#if IS_ENABLED(CONFIG_SCSI_UFS_CRYPTO)
 	bool crypto_enable;
 	u8 crypto_key_slot;
 	u64 data_unit_num;
+#endif /* CONFIG_SCSI_UFS_CRYPTO */
 
 	bool req_abort_skip;
 };
@@ -657,7 +659,6 @@ struct SEC_UFS_counting {
  * @crypto_capabilities: Content of crypto capabilities register (0x100)
  * @crypto_cap_array: Array of crypto capabilities
  * @crypto_cfg_register: Start of the crypto cfg array
- * @crypto_cfgs: Array of crypto configurations (i.e. config for each slot)
  * @ksm: the keyslot manager tied to this hba
  */
 struct ufs_hba {
@@ -857,7 +858,7 @@ struct ufs_hba {
 	 * This capability allows the host controller driver to use the
 	 * inline crypto engine, if it is present
 	 */
-#define UFSHCD_CAP_CRYPTO (1 << 6)
+#define UFSHCD_CAP_CRYPTO (1 << 7)
 
 	/* Allow only hibern8 without clk gating */
 #define UFSHCD_CAP_FAKE_CLK_GATING (1 << 6)
@@ -889,7 +890,6 @@ struct ufs_hba {
 	union ufs_crypto_capabilities crypto_capabilities;
 	union ufs_crypto_cap_entry *crypto_cap_array;
 	u32 crypto_cfg_register;
-	union ufs_crypto_cfg_entry *crypto_cfgs;
 	struct keyslot_manager *ksm;
 #endif /* CONFIG_SCSI_UFS_CRYPTO */
 
