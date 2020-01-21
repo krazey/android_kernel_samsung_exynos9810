d0bcdc305ad95d2931d050d78ae5a083225ea96d
Eric Biggers <ebiggers@google.com>
2020-01-21 09:25:59 -0800

    ANDROID: ext4, f2fs: enable direct I/O with inline encryption
    ext4 and f2fs have traditionally not supported direct I/O on encrypted
    files, since it's difficult to implement with the traditional
    filesystem-layer encryption.  But when inline encryption is used
    instead, it's straightforward to support direct I/O, as long as the I/O
    is fully filesystem-block-aligned.  Add support for it by:
    
    - Making the two generic direct I/O implementations in the kernel,
      __blockdev_direct_IO() and iomap_dio_rw(), set the encryption context
      on bios for inline-encrypted files.  __blockdev_direct_IO() is used by
      f2fs, and was used by ext4 in kernel v5.4 and earlier.  iomap_dio_rw()
      is used by ext4 in kernel v5.5 and later.
    
    - Making ext4 and f2fs allow direct I/O to encrypted files (rather the
      current behavior of falling back to buffered I/O) when the file is
      using inline encryption and the I/O is fully filesystem-block-aligned.
    
    Bug: 137270441
    Change-Id: I4c8f7497eb8f829d03611d24281113d68c21d4d1
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Satya Tangirala <satyat@google.com>
    
    Conflicts:
    	fs/direct-io.c
    	fs/iomap.c
    
    Signed-off-by: krazey <admin@krazey.de>

--- fs/iomap.c
+++ fs/iomap.c
@@ -14,6 +14,7 @@
 #include <linux/module.h>
 #include <linux/compiler.h>
 #include <linux/fs.h>
+#include <linux/fscrypt.h>
 #include <linux/iomap.h>
 #include <linux/uaccess.h>
 #include <linux/gfp.h>
