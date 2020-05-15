9a3842ccf5f43db021a96857a859f6504174e3b4
Jaegeuk Kim <jaegeuk@google.com>
2020-05-15 08:04:29 -0700

    Revert "f2fs: refactor resize_fs to avoid meta updates in progress"
    This reverts commit 7529068a775e71f7c6f70149f70c6031a79daeb3.

--- include/trace/events/f2fs.h
+++ include/trace/events/f2fs.h
@@ -46,7 +46,6 @@ TRACE_DEFINE_ENUM(CP_RECOVERY);
 TRACE_DEFINE_ENUM(CP_DISCARD);
 TRACE_DEFINE_ENUM(CP_TRIMMED);
 TRACE_DEFINE_ENUM(CP_PAUSE);
-TRACE_DEFINE_ENUM(CP_RESIZE);
 
 #define show_block_type(type)						\
 	__print_symbolic(type,						\
@@ -129,8 +128,7 @@ TRACE_DEFINE_ENUM(CP_RESIZE);
 		{ CP_RECOVERY,	"Recovery" },				\
 		{ CP_DISCARD,	"Discard" },				\
 		{ CP_PAUSE,	"Pause" },				\
-		{ CP_TRIMMED,	"Trimmed" },				\
-		{ CP_RESIZE,	"Resize" })
+		{ CP_TRIMMED,	"Trimmed" })
 
 #define show_fsync_cpreason(type)					\
 	__print_symbolic(type,						\
