c357f1f5fc546ccdc7e6a493a3933a74f57d5d20
krazey <admin@krazey.de>
2021-11-23 11:13:06 +0000

    Revert "f2fs: clear nlink if fail to add_link"
    This reverts commit ad48a5fcb15397e6f632fe15433734f7dce32667.

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -771,18 +771,6 @@ void f2fs_handle_failed_inode(struct inode *inode)
 	f2fs_update_inode_page(inode);
 	f2fs_inode_synced(inode);
 
-	/*
-	 * clear nlink of inode in order to release resource of inode
-	 * immediately.
-	 */
-	clear_nlink(inode);
-
-	/*
-	 * we must call this to avoid inode being remained as dirty, resulting
-	 * in a panic when flushing dirty inodes in gdirty_list.
-	 */
-	update_inode_page(inode);
-
 	/* don't make bad inode, since it becomes a regular file. */
 	unlock_new_inode(inode);
 
