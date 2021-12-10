64c5ea92a941a21592510268dfbe7db9a49ddc4f
krazey <admin@krazey.de>
2021-12-10 13:41:36 +0000

    exynos9810: arm64: remove the specific definition of set_dma_ops()
    48483ef7013fdba080e2995ca3e1b27ecb1ba889: treewide: Consolidate set_dma_ops() implementations
    
    Signed-off-by: krazey <admin@krazey.de>

--- arch/arm64/include/asm/dma-mapping.h
+++ arch/arm64/include/asm/dma-mapping.h
@@ -53,11 +53,6 @@ static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 		return __generic_dma_ops(NULL);
 }
 
-static inline void set_dma_ops(struct device *dev, struct dma_map_ops *ops)
-{
-	dev->dma_ops = ops;
-}
-
 void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 			const struct iommu_ops *iommu, bool coherent);
 #define arch_setup_dma_ops	arch_setup_dma_ops
