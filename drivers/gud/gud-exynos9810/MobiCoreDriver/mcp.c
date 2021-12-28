5aab2bb6699ab376e8ea96e0e764646f36ef527c
krazey <admin@krazey.de>
2021-12-28 15:31:13 +0100

    [exynos9810] hotfix: drivers: gpu: <linux/sched.h> (revert for 4.11)
    Signed-off-by: krazey <admin@krazey.de>

--- drivers/gud/gud-exynos9810/MobiCoreDriver/mcp.c
+++ drivers/gud/gud-exynos9810/MobiCoreDriver/mcp.c
@@ -28,7 +28,7 @@
 #include <asm/barrier.h>
 #include <linux/irq.h>
 #include <linux/version.h>
-#if KERNEL_VERSION(4, 11, 0) <= LINUX_VERSION_CODE
+#if KERNEL_VERSION(4, 10, 0) <= LINUX_VERSION_CODE
 #include <linux/sched/clock.h>	/* local_clock */
 #endif
 
