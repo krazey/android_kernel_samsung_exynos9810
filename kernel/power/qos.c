ea1724de7e4e08d0b9eb1fdf2fbf96bd8874a335
krazey <admin@krazey.de>
2022-04-12 02:10:52 +0200

    [exynos9810] drivers: gpu: initial code for b_r26p0-01eac0
    Signed-off-by: krazey <admin@krazey.de>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -440,6 +440,34 @@ static struct pm_qos_object fsys0_throughput_max_pm_qos = {
 };
 #endif
 
+#ifdef CONFIG_MALI_PM_QOS
+static BLOCKING_NOTIFIER_HEAD(gpu_freq_min_notifier);
+static struct pm_qos_constraints gpu_freq_min_constraints = {
+	.list = PLIST_HEAD_INIT(gpu_freq_min_constraints.list),
+	.target_value = PM_QOS_GPU_FREQ_MIN_DEFAULT_VALUE,
+	.default_value = PM_QOS_GPU_FREQ_MIN_DEFAULT_VALUE,
+	.type = PM_QOS_MAX,
+	.notifiers = &gpu_freq_min_notifier,
+};
+static struct pm_qos_object gpu_freq_min_pm_qos = {
+	.constraints = &gpu_freq_min_constraints,
+	.name = "gpu_freq_min",
+};
+
+static BLOCKING_NOTIFIER_HEAD(gpu_freq_max_notifier);
+static struct pm_qos_constraints gpu_freq_max_constraints = {
+	.list = PLIST_HEAD_INIT(gpu_freq_max_constraints.list),
+	.target_value = PM_QOS_GPU_FREQ_MAX_DEFAULT_VALUE,
+	.default_value = PM_QOS_GPU_FREQ_MAX_DEFAULT_VALUE,
+	.type = PM_QOS_MIN,
+	.notifiers = &gpu_freq_max_notifier,
+};
+static struct pm_qos_object gpu_freq_max_pm_qos = {
+	.constraints = &gpu_freq_max_constraints,
+	.name = "gpu_freq_max",
+};
+#endif
+
 static struct pm_qos_object *pm_qos_array[] = {
 	&null_pm_qos,
 	&cpu_dma_pm_qos,
@@ -475,6 +503,10 @@ static struct pm_qos_object *pm_qos_array[] = {
 	&iva_throughput_max_pm_qos,
 	&score_throughput_max_pm_qos,
 	&fsys0_throughput_max_pm_qos,
+#endif
+#ifdef CONFIG_MALI_PM_QOS
+	&gpu_freq_min_pm_qos,
+	&gpu_freq_max_pm_qos,
 #endif
 };
 
