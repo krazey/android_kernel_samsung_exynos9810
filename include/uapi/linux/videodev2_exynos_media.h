15380cd2cb510b6d20ac6cbd27aa11b26a432f48
krazey <admin@krazey.de>
2025-06-19 18:51:11 +0200

    include: uapi: linux: Import Exynos Linaro BSP kernel headers
    * Directly from `hardware/samsung_slsi-linaro/exynos/kernel-4.14-headers/kernel-headers/videodev2_exynos_media.h`
    * For building `hardware/samsung_slsi-linaro/exynos` components after Blueprint transition.
    
    Change-Id: Ie8887a7de3d145e34208b40ed45b3e359980652a
    
    Signed-off-by: krazey <admin@krazey.de>

--- include/uapi/linux/videodev2_exynos_media.h
+++ include/uapi/linux/videodev2_exynos_media.h
@@ -31,6 +31,18 @@
 #define V4L2_CID_CSC_EQ			(V4L2_CID_EXYNOS_BASE + 101)
 #define V4L2_CID_CSC_RANGE		(V4L2_CID_EXYNOS_BASE + 102)
 
+/* for hardware/samsung_slsi-linaro/exynos inclusion */
+#define V4L2_PIX_FMT_NV12M_SBWC_8B  v4l2_fourcc('M', '1', 'S', '8')
+#define V4L2_PIX_FMT_NV12M_SBWC_10B v4l2_fourcc('M', '1', 'S', '1')
+#define V4L2_PIX_FMT_NV21M_SBWC_8B  v4l2_fourcc('M', '2', 'S', '8')
+#define V4L2_PIX_FMT_NV21M_SBWC_10B v4l2_fourcc('M', '2', 'S', '1')
+#define V4L2_PIX_FMT_NV12N_SBWC_8B  v4l2_fourcc('N', '1', 'S', '8')
+#define V4L2_PIX_FMT_NV12N_SBWC_10B v4l2_fourcc('N', '1', 'S', '1')
+#define V4L2_PIX_FMT_NV12M_SBWCL_8B v4l2_fourcc('M', '1', 'L', '8')
+#define V4L2_PIX_FMT_NV12M_SBWCL_10B    v4l2_fourcc('M', '1', 'L', '1')
+#define V4L2_PIX_FMT_NV12N_SBWCL_8B v4l2_fourcc('N', '1', 'L', '8')
+#define V4L2_PIX_FMT_NV12N_SBWCL_10B    v4l2_fourcc('N', '1', 'L', '1')
+
 /* for DRM playback scenario */
 #define V4L2_CID_CONTENT_PROTECTION	(V4L2_CID_EXYNOS_BASE + 201)
 
